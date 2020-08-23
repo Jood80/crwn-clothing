4686895320df9b00bef29b3c598e21e982e9eab2
Nujood <shawar.nujood@gmail.com>
2020-08-24 00:43:20 +0300

    setting up Routing and app.css style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import HomePage from './compnenets/homepage'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from './pages/signIn-signUp/signIn-signUp.component'
+import Header from './components/header/header.component'
+import "./App.css";
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
     </div>
   );
 }
